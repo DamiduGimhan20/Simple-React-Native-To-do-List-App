a48d57b1d64a626b86a81700200181c345a5f236
Damidu Gimhan Jayawardhana <104602789+DamiduGimhan20@users.noreply.github.com>
2024-04-28 15:53:20 +0530

    main commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import TodoScreeen from './src/screen/TodoScreeen';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+    <View>
+      <TodoScreeen />
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
